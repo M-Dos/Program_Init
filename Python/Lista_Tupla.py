3c8aaeaddeafcae776f7cb444c1e7494a8878345
Leilson Castilhos <scastilhos.leilson@gmail.com>
2021-07-14 13:07:01 -0300

    Create Lista_Tupla.py

---
+++ Python/Lista_Tupla.py
@@ -0,0 +1,5 @@
+nomes_paises = ['Brasil','Argentina','China','Canadá','Japão'] 
+print(nomes_paises)
+print('Tamanho da lista:', len(nomes_paises))
+print('País:', nomes_paises[4])
+print('País', nomes_paises[-1])
